ef752ae3c563e2d678761074d18bc3e57bafdb13
Thanh Tuan <tuan.dang@tekmedi.vn>
2021-12-15 08:47:38 +0700

    First

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return BadRequest();
+        }
+    }
+}
